a5a6dc86c37389840e312e4ebe078a52c29928ca
Renan Henrique Garcia Tito <renan.tito2@gmail.com>
2024-09-24 22:36:22 -0300

    chore!: changed application to springboot support

---
+++ carteira-digital/src/test/java/br/com/test/carteira_digital/CarteiraDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.test.carteira_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
