6577fdfb19a348381723b4c9b3691e70d40a6cf5
Renan Henrique Garcia Tito <renan.tito2@gmail.com>
2024-09-27 02:45:30 -0300

    test:  TipoEventoTest Positivo/Negativo

---
+++ carteira-digital/src/test/java/br/com/test/carteira_digital/model/enums/TipoEventoTest.java
@@ -0,0 +1,27 @@
+package br.com.test.carteira_digital.model.enums;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class TipoEventoTest {
+
+    @Test
+    public void testEventoPositivo() {
+        for (int idTipoEvento = 1; idTipoEvento <= 5; idTipoEvento++) {
+            TipoEvento tipoEvento = TipoEvento.fromCodigo(idTipoEvento);
+            Assertions.assertNotNull(tipoEvento, "TipoEvento para o código " + idTipoEvento + " não deve ser nulo.");
+            Assertions.assertEquals(idTipoEvento, tipoEvento.codigo, "O código do evento retornado deve ser igual ao esperado.");
+        }
+    }
+
+    @Test
+    public void testEventoNegativo() {
+        int codigoInvalido = 999; // Um número que não corresponde a nenhum tipo válido
+        Assertions.assertThrows(IllegalArgumentException.class, () -> TipoEvento.fromCodigo(codigoInvalido)) ;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> TipoEvento.fromCodigo(null)) ;
+    }
+
+}
